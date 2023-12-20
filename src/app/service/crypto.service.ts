54c884404cdd1f9f79b6b5e114f798bed29fd82b
dev-araujo <theriancirculo@protonmail.com>
2023-12-20 18:20:23 -0300

    style:responsive banner

--- src/app/service/crypto.service.ts
+++ src/app/service/crypto.service.ts
@@ -1,8 +1,18 @@
-import { HttpClient } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  catchError,
+  switchMap,
+  throwError,
+} from 'rxjs';
 
-import { ACCESSTOKEN } from '../../../config/config';
+import { ACCESSTOKEN, ACCESSTOKENBACKUP } from '../../../config/config';
 import { CoinList, Currency } from '../models/shared.interface';
 
 @Injectable({
@@ -10,11 +20,12 @@ import { CoinList, Currency } from '../models/shared.interface';
 })
 export class CryptoService {
   private readonly baseUrl = 'https://api.coinranking.com/v2/';
+  private currentAccessToken = ACCESSTOKEN;
+  private fiatSubject = new BehaviorSubject<Currency>({ name: '', code: '' });
+
+  fiat$ = this.fiatSubject.asObservable();
 
   constructor(private http: HttpClient) {}
-
-  private fiatSubject = new BehaviorSubject<Currency>({ name: '', code: '' });
-  fiat$ = this.fiatSubject.asObservable();
 
   sharedFiat(changeFiat: Currency) {
     this.fiatSubject.next(changeFiat);
@@ -27,10 +38,19 @@ export class CryptoService {
     const options = {
       headers: {
         'Content-Type': 'application/json',
-        'x-access-token': ACCESSTOKEN,
+        'x-access-token': this.currentAccessToken,
       },
     };
     const endpoint = `${this.baseUrl}coins?referenceCurrencyUuid=${currency}&orderBy=price&limit=10&offset=${offset}`;
-    return this.http.get<CoinList | any>(endpoint, options);
+    return this.http.get<CoinList | any>(endpoint, options).pipe(
+      catchError((error) => {
+        if (error instanceof HttpErrorResponse && error.status === 429) {
+          this.currentAccessToken = ACCESSTOKENBACKUP;
+          options.headers['x-access-token'] = this.currentAccessToken;
+          return this.http.get<CoinList | any>(endpoint, options);
+        }
+        return throwError(error);
+      })
+    );
   }
 }
