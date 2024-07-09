9e4414bd099e74d6fc7dcdaec4b8cfd2b6a3d4da
dev-araujo <theriancirculo@protonmail.com>
2024-07-09 08:29:23 -0300

    refac.: inject services in chartService

--- src/app/shared/components/graphic/service/chart.service.ts
+++ src/app/shared/components/graphic/service/chart.service.ts
@@ -1,10 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CryptoService } from '../../../../service/general/crypto.service';
+import { StateService } from '../../../../service/state/state.service';
+import { Currency, HistoricalData } from '../../../../models/shared.model';
 import Chart from 'chart.js/auto';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChartService {
+  constructor(
+    private cryptoService: CryptoService,
+    private stateService: StateService
+  ) {}
+
+  getFiat(): Observable<Currency> {
+    return this.stateService.fiat$;
+  }
+
+  getSymbol(id: string): Observable<string> {
+    return this.cryptoService.getSymbol(id);
+  }
+
+  getCoinHistory(
+    id: string,
+    period: string,
+    currency: string | any
+  ): Observable<HistoricalData> {
+    return this.cryptoService.getCoinHistory(id, period, currency);
+  }
+
   createChart(context: CanvasRenderingContext2D, data: any): Chart {
     return new Chart(context, {
       type: 'line',
