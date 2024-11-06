439af0c0f85e556a138d5dd0ea1d162abceb3fff
dev-araujo <theriancirculo@protonmail.com>
2024-11-06 16:26:34 -0300

    feat: auth button

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-import { DropdownModule } from 'primeng/dropdown';
-import { InputTextModule } from 'primeng/inputtext';
-import { TooltipModule } from 'primeng/tooltip';
-
-import { Currency } from '../../../models/shared.model';
 import { Router, RouterLink, RouterModule } from '@angular/router';
+
+import { AuthService } from '../../../service/auth/auth.service';
+import { ButtonModule } from 'primeng/button';
+import { Component } from '@angular/core';
+import { Currency } from '../../../models/shared.model';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { NgIf } from '@angular/common';
 import { StateService } from '../../../service/state/state.service';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-header',
@@ -19,6 +21,8 @@ import { StateService } from '../../../service/state/state.service';
     FormsModule,
     RouterLink,
     RouterModule,
+    NgIf,
+    ButtonModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
@@ -31,7 +35,10 @@ export class HeaderComponent {
   fiat = this.currency[1];
   find = '';
 
-  constructor(private router: Router, private stateService: StateService) {}
+  constructor(
+    private stateService: StateService,
+    private authService: AuthService
+  ) {}
 
   getFiat(event: Currency): void {
     this.stateService.sharedFiat(event);
@@ -47,5 +54,16 @@ export class HeaderComponent {
     if (this.find === '') {
       this.stateService.sharedSearch(this.find);
     }
+  }
+
+  account: string | null = null;
+
+  async connect() {
+    this.account = await this.authService.connect();
+  }
+
+  disconnect() {
+    this.authService.disconnect();
+    this.account = null;
   }
 }
