37426a4f61ed095d1091f9da3865ee79ed2a9178
araujo <theriancirculo@protonmail.com>
2025-10-17 11:29:07 -0300

    del: all project

--- src/app/service/auth/messages.ts
+++
@@ -1,5 +0,0 @@
-export const messages = {
-  notFound: 'MetaMask não foi encontrado. Por favor, instale a extensão.',
-  notAvailable: 'Alerta: MetaMask não está disponível.',
-  error: (err: any) => `Erro ao conectar à MetaMask:${err}`,
-};
--- src/app/shared/pipes/iconUrlPipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'iconUrl', standalone: true })
-export class IconUrlPipe implements PipeTransform {
-  transform(iconUrl: string): string {
-    return `${iconUrl}?size=30x30`;
-  }
-}
